c18ef4c17bb99c2a0fa052b843d76bad75622893
yanghangfeng <1437183653@qq.com>
2016-12-30 20:02:28 +0800

    update one_img.py

--- spiderFile/one_img.py
+++ spiderFile/one_img.py
@@ -1,18 +1,37 @@
 import re
 import requests
 
-temp = 'http://caodan.org/page/'
+temp = 'http://wufazhuce.com/one/'
 count = 1
-for i in range(1, 1331):
+for i in range(14, 1580):
     url = temp + str(i)
     page = requests.get(url).text
     reg = re.compile('src="(http://.*?\.jpg)"')
     img_url = re.findall(reg, page)
     if img_url != []:
-        with open('E:/img/%s.jpg' % count, 'wb') as file:
-            img_data = requests.get(img_url[0]).content
-            file.write(img_data)
-            count += 1
-    else:
-        continue
+        with open('./{}.jpg'.format(count), 'wb') as file:
+            try:
+                img_data = requests.get(img_url[0]).content
+                file.write(img_data)
+                count += 1
+            except:
+                pass
 print('OK!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
